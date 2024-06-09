7ae5b96e7315b4c91d27093508dcdabf5626f5e6
Aayush <singhaayush2034@gmail.com>
2024-06-09 12:01:50 +0530

    completed

---
+++ script.js
@@ -0,0 +1,40 @@
+setInterval(function () {
+  let currentDateTime = new Date();
+  // Date
+  let currentDate = currentDateTime.getDate();
+  let currentMonth = currentDateTime.getMonth();
+  let currentYear = currentDateTime.getFullYear();
+  if (currentDate < 10) {
+    currentDate = "0" + currentDate;
+  }
+  if (currentMonth < 10) {
+    currentMonth = "0" + currentMonth;
+  }
+
+  document.getElementById("date").innerHTML =
+    currentDate + "/" + currentMonth + "/" + currentYear;
+  // Time
+  let currentHour = currentDateTime.getHours();
+  let currentMinute = currentDateTime.getMinutes();
+  let currentSecond = currentDateTime.getSeconds();
+  let period = "AM";
+  //   condition for AM or PM
+  if (currentHour >= 12) {
+    period = "PM";
+  }
+  //   conditions for 12 Hours Time
+  if (currentHour > 12) {
+    currentHour = currentHour - 12;
+  }
+  if (currentHour < 10) {
+    currentHour = "0" + currentHour;
+  }
+  if (currentMinute < 10) {
+    currentMinute = "0" + currentMinute;
+  }
+  if (currentSecond < 10) {
+    currentSecond = "0" + currentSecond;
+  }
+  document.getElementById("time").innerHTML =
+    currentHour + ":" + currentMinute + ":" + currentSecond + period;
+});
